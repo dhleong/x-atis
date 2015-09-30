1c355acd6cbdf6e56931df1ee48427655920f1ec
dhleong <falcone88@gmail.com>
2015-09-30 10:21:58 -0400

    Use mary-tts to generate speech
    Closes #1

--- project.clj
+++ project.clj
@@ -3,11 +3,18 @@
   :url "http://github.com/dhleong/xatis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"fast-md5" "https://dl.bintray.com/fundacionjala/enforce"
+                 "mary-tts" "https://dl.bintray.com/marytts/marytts/"
+                 "mary-snapshots" "https://oss.jfrog.org/artifactory/oss-snapshot-local"
+                 "jtok" "https://dl.bintray.com/dfki-lt/maven/"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.zip "0.1.1"]
                  [asfiled "0.1.0-SNAPSHOT"]
-                 [clj-time "0.11.0"]]
+                 [clj-time "0.11.0"]
+                 [de.dfki.mary/marytts-runtime "5.2-SNAPSHOT"]
+                 [de.dfki.mary/marytts-lang-en "5.2-SNAPSHOT"]
+                 [de.dfki.mary/voice-cmu-slt-hsmm "5.2-SNAPSHOT"]]
   :main ^:skip-aot xatis.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
